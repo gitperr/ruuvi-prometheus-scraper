4e8f90554c9ff3552871be23316c132e75502b9c
Alper Polat <gitperr@gmail.com>
2024-01-28 22:00:51 +0200

    Runs in docker on my main machine
    Still one warning:
    app/main.py:49: DeprecationWarning: There is no current event loop
      asyncio.get_event_loop().run_until_complete(main())
    
    and remember the privileged mode it requires to run...
    
    Signed-off-by: Alper Polat <gitperr@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,53 +1,49 @@
-#!/usr/bin/env python3
+import os
+import asyncio
+from datetime import datetime
 
-import time
-import pprint
-import signal
-import sys
+os.environ["RUUVI_BLE_ADAPTER"] = "bleak"
 from prometheus_client import Gauge, start_http_server
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-
-beacons = {
-    "D4:EF:90:6E:11:4F": {
-        "name": "Bedroom",
-        "last_update": 0.0,
-        "sensor_data": {},
-    },
-    "F8:50:A3:13:1D:B7": {
-        "name": "Livingroom",
-        "last_update": 0.0,
-        "sensor_data": {},
-    },
-}
 
 temp_gauge = Gauge("ruuvi_temperature_c", "Temperature in Celsius", ["location"])
 humidity_gauge = Gauge("ruuvi_humidity_percent", "Humidity %", ["location"])
 pressure_gauge = Gauge("ruuvi_pressure_hpa", "Air pressure hPa", ["location"])
 battery_gauge = Gauge("ruuvi_battery_v", "Battery V", ["location"])
 
-
-def handle_data(data):
-    [mac, sensor_data] = data
-    beacon = beacons[mac]
-    beacon["last_update"] = time.time()
-    beacon["sensor_data"] = sensor_data
-    location = beacon["name"]
-    temp_gauge.labels(location).set(sensor_data["temperature"])
-    humidity_gauge.labels(location).set(sensor_data["humidity"] / 100.0)
-    pressure_gauge.labels(location).set(sensor_data["pressure"])
-    battery_gauge.labels(location).set(sensor_data["battery"] / 1000.0)
+sensors = {
+    "Livingroom": "F8:50:A3:13:1D:B7",
+    "Bedroom": "D4:EF:90:6E:11:4F",
+}
 
 
-def sigint_handler(signal, frame):
-    print("Collected data:")
-    pprint.pprint(beacons)
-    sys.exit(130)
+def set_gauges(data: tuple, room: str):
+    """Sets gauges for Prometheus to scrape"""
+    temp = data[1]["temperature"]
+    humidity = data[1]["humidity"]
+    pressure = data[1]["pressure"]
+    battery = data[1]["battery"]
+
+    temp_gauge.labels(room).set(temp)
+    humidity_gauge.labels(room).set(humidity / 100.0)
+    pressure_gauge.labels(room).set(pressure)
+    battery_gauge.labels(room).set(battery / 1000.0)
+
+
+async def main():
+    """Async function that runs and gathers data from RuuviTags"""
+    async for data in RuuviTagSensor.get_data_async():
+        sensor_mac = data[0]
+        if sensor_mac == sensors["Livingroom"]:
+            room = "Livingroom"
+            set_gauges(data, room)
+        elif sensor_mac == sensors["Bedroom"]:
+            room = "Bedroom"
+            set_gauges(data, room)
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, sigint_handler)
     print("Starting HTTP server for Prometheus scraping on localhost:8000")
     start_http_server(8000)
-
     print("Reading data from Ruuvi tags")
-    RuuviTagSensor.get_datas(handle_data, beacons.keys())
+    asyncio.get_event_loop().run_until_complete(main())
